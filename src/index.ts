90ae5d554e0f491ea4558987560b80f069c366ee
Alexander Balyanov <alex44298@yandex.ru>
2025-07-14 11:50:52 +0500

    начальная версия проекта

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import "./styles/styles.css";
+import { todos } from "./utils/constants";
+
+const contentElement = document.querySelector('.todos__list');
+
+const template = document.querySelector('#todo-item-template') as HTMLTemplateElement;
+
+const createElement = (name: string) => {
+  const itemElement = template.content.querySelector('.todo-item').cloneNode(true) as HTMLElement;
+  const title = itemElement.querySelector('.todo-item__text');
+  title.textContent = name;
+  return itemElement;
+};
+
+todos.forEach((item) => {
+  const itemElement = createElement(item);
+  contentElement.prepend(itemElement);
+});
